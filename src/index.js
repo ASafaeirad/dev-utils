41cedc4ebb0ce2fbb59a13da9c7f738b43ae2df8
frontendmonster <frontendmonster@gmail.com>
2018-09-25 04:35:21 +0330

    Add webpack utils

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as env } from './env';
 export { default as clear } from './clear';
 export { chalks, morgan, Signale } from './logger';
+export { generateWebpackRules } from './webpack';
---
+++ src/webpack/index.js
@@ -0,0 +1,1 @@
+export { default as generateWebpackRules } from './rules';
